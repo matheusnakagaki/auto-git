9740d586f278e296865bed2fc51566432a9816e5
Matheus Gouveia <matheusnakagaki@gmail.com>
2026-01-28 11:43:46 -0300

    Add fzf logic with git command

--- auto-git.sh
+++ auto-git.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-hello="Hello world!"
 
-echo "$hello"
+selected=$(git branch | fzf \
+    --height 40% \
+    --layout reverse \
+    --border \
+    --preview \
+        'git log --oneline')
+
+selected=$(echo $selected | tr -d '* ')
+
+echo "$selected"
+
+git switch "$selected"
